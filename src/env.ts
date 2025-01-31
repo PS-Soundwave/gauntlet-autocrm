43210c82704e9b0163e5c93aa3037be260e5f05e
Soundwave <PS-Soundwave@users.noreply.github.com>
2025-01-31 14:45:33 -0500

    Add env validation

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,10 @@ export const env = createEnv({
     server: {
         ORIGIN: z.string().url(),
         DATABASE_URL: z.string().url(),
-        LANGCHAIN_API_KEY: z.string()
+        OPENAI_API_KEY: z.string(),
+        LANGCHAIN_API_KEY: z.string(),
+        LANGCHAIN_PROJECT: z.string(),
+        LANGCHAIN_TRACING_V2: z.boolean({ coerce: true })
     },
     client: {
         NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
@@ -14,7 +17,10 @@ export const env = createEnv({
     runtimeEnv: {
         ORIGIN: process.env.ORIGIN,
         DATABASE_URL: process.env.DATABASE_URL,
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
         LANGCHAIN_API_KEY: process.env.LANGCHAIN_API_KEY,
+        LANGCHAIN_PROJECT: process.env.LANGCHAIN_PROJECT,
+        LANGCHAIN_TRACING_V2: process.env.LANGCHAIN_TRACING_V2,
         NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
         NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
     }
